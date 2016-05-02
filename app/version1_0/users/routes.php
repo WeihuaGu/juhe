ffc18e63d6bb142bb90c0dd8d07d1fac251966cc
weihuagu <ministarmail@msn.cn>
2016-05-02 17:03:01 +0800

    o

--- app/version1_0/users/routes.php
+++ app/version1_0/users/routes.php
@@ -9,8 +9,16 @@ $app->get('/v1_0/users', function ($request, $response, $args) {
     // Render index view
     //return $this->renderer->render($response, 'juhehelp.phtml', $args);
 })->setName('users');
-$app->put('/v1_0/users', function ($request, $response, $args) {
+$app->post('/v1_0/users', function ($request, $response, $args) {
 	$data = array('Status' => 'created', 'userid' =>'0000');
+        $jsonresponse=$response->withJson($data, 201);
+	return $jsonresponse;
+});
+
+$app->get('/v1_0/users/{id:[0-9]+}', function ($request, $response, $args) {
+      $data = array('name' => 'ali', "updatedAt"=>"2015-07-14T02:31:50.100Z",
+  "phone"=>"18612340000");
+	echo $args['id'];
         $jsonresponse=$response->withJson($data, 201);
 	return $jsonresponse;
 });
