fec6150cf86ef14c7920d1d8db68d7e9139089bb
weihuagu <ministarmail@msn.cn>
2016-05-02 16:05:53 +0800

    o

--- app/version1_0/users/routes.php
+++ app/version1_0/users/routes.php
@@ -1,16 +1,17 @@
 <?php
-/**
-请实例化控制器，并且在控制器中获取到模型数据，然后传递给$args数组
 
-
-**/
 namespace app\version1_0\users;
 // Routes
-$app->get('/users', function ($request, $response, $args) {
+$app->get('/1.0/users', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/users' route");
-     echo '<h1> users test! </h1>';
+     echo '<h1> this is the users restful api</h1>';
     // Render index view
     //return $this->renderer->render($response, 'juhehelp.phtml', $args);
 })->setName('users');
+$app->put('/1.0/users', function ($request, $response, $args) {
+	$data = array('Status' => 'created', 'userid' =>'0000');
+        $jsonresponse=$response->withJson($data, 201);
+	return $jsonresponse;
+});
 
