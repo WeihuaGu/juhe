53bc2962005911051da204eb4c65a8665c9658f2
weihuagu <ministarmail@msn.cn>
2016-05-02 16:12:00 +0800

    o

--- app/version1_0/users/routes.php
+++ app/version1_0/users/routes.php
@@ -2,14 +2,14 @@
 
 namespace app\version1_0\users;
 // Routes
-$app->get('/1.0/users', function ($request, $response, $args) {
+$app->get('/v1_0/users', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/users' route");
      echo '<h1> this is the users restful api</h1>';
     // Render index view
     //return $this->renderer->render($response, 'juhehelp.phtml', $args);
 })->setName('users');
-$app->put('/1.0/users', function ($request, $response, $args) {
+$app->put('/v1_0/users', function ($request, $response, $args) {
 	$data = array('Status' => 'created', 'userid' =>'0000');
         $jsonresponse=$response->withJson($data, 201);
 	return $jsonresponse;
